132e5e9097231d3e7e7c985a508280bf9e9f523e
JornBrink <jornbrink@kpnmail.nl>
2022-09-08 16:52:18 +0200

    Update Multiplateserial1.R

--- upstream (R) processors/Rscript development/Update Rscript development/Multiplateserial1.R
+++ upstream (R) processors/Rscript development/Update Rscript development/Multiplateserial1.R
@@ -2,3 +2,30 @@ library(dplyr)
 library(rlist)
 library(readxl)
 library(readr)
+
+#Get the filename and parse it into seperate parts
+main<- read_xlsx(file_path, file_name="")
+stockList <- tryCatch({
+  GetStockList(file_path)
+},
+error = function(cond){
+  if(errMessage == ""){
+    errMessage <<- "Input file error - stockList"
+  }
+  return(NA)
+})
+
+#get the drug concentrations(stocklist)
+GetStocklist <- function(file_name){
+  stocks<<-read_xlsx(file_name)
+}
+
+
+
+
+
+# #TROUBLESHOOTING---------
+errMessage <<- ""
+fpath <- "C:\\Users\\jornb\\OneDrive\\Work\\Maik"
+dataName <- "MV_InputTemplate.xlsx"
+dqs <- main(paste(fpath, dataName, sep="//"))
